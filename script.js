c5f071eea37d75a8ca526fe742c242ebca2b51d1
Wade <wadefh@gmail.com>
2021-12-29 19:45:01 -0800

    reworked ui and added propper player generation

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+//factory function creates the players and keeps their score
+const createPlayer = (name, symbol) => {
+    let score = 0;
+    return{name, symbol, score}
+};
+
 //gameboard array
 const gameboard = (() => {
     let board = [];
+    //combinations of  board game array index that win game
+    const winConditions =[ 
+        [0,1,2], [3,4,5], [6,7,8], //horizontal
+        [0,3,6], [1,4,7], [2,5,8], //vertical
+        [0,4,8], [2,4,6]           //diagonal
+    ];
 
     const changeState = function(player, index){
         board[index] = player;
@@ -11,17 +23,27 @@ const gameboard = (() => {
     }
     const reset = function() {
     }
-    return {changeState, getBoardIndex, reset};
+
+    //check against array of win conditions to see if won 
+    const gameWon = function(){
+        for(id in winConditions){
+            if(  board[winConditions[id][0]] == board[winConditions[id][1]] && 
+                board[winConditions[id][0]] == board[winConditions[id][2]] && 
+                board[winConditions[id][0]] != undefined){
+                        console.log("gameover");
+                        return true;}
+           }     
+           return false;   
+        }   
+
+    return {changeState, getBoardIndex, reset, gameWon};
 })();
 
-//factory function creates the players and keeps their score
-const player = (name, symbol) => {
-    return{name, symbol}
-};
-
-const gameDOM = (() => {
+//module handle game play
+const game = (() => {
+    let player = [];
     let playerType;
-    let symbol;
+    let turn = 0;
 
     //cache DOM
     const startBoxButtons = document.querySelectorAll('button');
@@ -31,10 +53,27 @@ const gameDOM = (() => {
     const gameboardDOM = document.querySelector('.gameBoard');
     const nameInput = playerNameMenuDOM.querySelectorAll('input'); 
 
-    //bind event   
-    gameboardDOM.addEventListener('click', (event) => game.playGame(event.target));
+    //bind buttons       
     startBoxButtons.forEach(button => button.addEventListener('click', (event) => buttonPress(event.target)));
-   
+
+    const playGame = function (target){        
+        console.log(target.id);
+        gameboard.changeState(player[turn].symbol, target.id);
+        if(gameboard.gameWon()){
+            console.log(`${player[turn].name} wins!`)            
+        }
+        else{   
+            turn = (turn == 0)? 1: 0;
+        }        
+        renderBoard(target);      
+    };
+
+    //draw symbol to clicked square
+    const renderBoard = function (targetHTML) {
+        targetHTML.innerText = gameboard.getBoardIndex(targetHTML.id);
+        }     
+        
+           
     function buttonPress(target){
         switch(target.id){
             case "vsAI":
@@ -43,23 +82,21 @@ const gameDOM = (() => {
                 toggleHidden(playerNameMenuDOM);
                 break;
             case "vsPlayer":
-                playerType = "player";
+                playerType = "human";
                 toggleHidden(playerSelectMenuDOM);
                 toggleHidden(playerNameMenuDOM);
                 break;
-            case "play":
-                symbol = 'X';
-                playerone = player(nameInput[0].value, symbol);
-                symbol = 'O';
-                playertwo = player(nameInput[1].value, symbol);
+            case "play":               
+                player[0] = createPlayer(nameInput[0].value, 'X');
+                player[1] = createPlayer(nameInput[1].value, 'O');
                 toggleHidden(menuBoxDOM);
-                toggleHidden(gameboardDOM);
+                //bind board
+                gameboardDOM.addEventListener('click', (event) => playGame(event.target));
                 break;
             default:
                 break;
         }
-    }
-    
+    }    
     //toggle hidden    
     function toggleHidden(target){
         if(target.classList.contains('hidden')){
@@ -69,47 +106,4 @@ const gameDOM = (() => {
             target.classList.add('hidden');
         }
     }
-    //draw symbol to clicked square
-    const renderBoard = function (targetHTML) {
-        targetHTML.innerText = gameboard.getBoardIndex(targetHTML.id);
-        }
-    return {renderBoard};      
-})();
-
-//module handle game logic
-const game = (() => {
-    //combinations of  board game array index that win game
-    const winConditions =[ 
-        [0,1,2], [3,4,5], [6,7,8], //horizontal
-        [0,3,6], [1,4,7], [2,5,8], //vertical
-        [0,4,8], [2,4,6]           //diagonal
-    ];
-    let gameWon = false
-    let players;
-
-    const getPlayer = function (name) {
-
-    }
-
-    const playGame = function (target){            
-        console.log(target.id);
-        gameboard.changeState(symbol, target.id);
-        if(gameWon == false){gameDOM.renderBoard(target);}
-        gameState();       
-        symbol = (symbol == "X")? "O": "X";
-    };
-
-    //check against array of win conditions to see if won 
-    function gameState(){
-       for(id in winConditions){
-       if(  gameboard.getBoardIndex(winConditions[id][0]) == gameboard.getBoardIndex(winConditions[id][1]) && 
-            gameboard.getBoardIndex(winConditions[id][0]) == gameboard.getBoardIndex(winConditions[id][2]) && 
-            gameboard.getBoardIndex(winConditions[id][0]) != undefined){
-                console.log("gameover");
-                gameWon = true;
-            }     
-       }
-
-    }
-    return {playGame};
 })();
